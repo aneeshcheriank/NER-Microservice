3ade8acb4d3afd8c78ae3fdbc958c930315dfad8
aneeshcheriank <aneeshcheriank@gmail.com>
2024-12-31 22:55:37 +0100

    write the entities extracted to a file

--- src/ner.py
+++ src/ner.py
@@ -6,7 +6,7 @@ import json
 from wiki import search_wiki
 from engine import entity_recognition
 
-def ner(page):
+def ner(page, do_print=False):
     """
     Extract entities from a wiki article
     input: query(string)
@@ -16,12 +16,16 @@ def ner(page):
     content = search_wiki(page)
     entities = entity_recognition(content)
 
+    if do_print:
+        print(json.dumps(entities, indent=2))
+
     return entities
 
+def write_json(data, filename="output.json"):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=2)
+
 if __name__ == "__main__":
-    print(
-        json.dumps(
-            ner("Donald Trump"),
-            indent=2
-        )
-    )
+    query = "Benjamin Netanyahu"
+    entities = ner(query, do_print=True)
+    write_json(entities)
